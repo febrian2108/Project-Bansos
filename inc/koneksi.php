ebfdd10a11411ef620f8c1e3707878f2c373480f
unknown <fahrifebrian93@gmail.com>
2024-04-23 09:45:04 +0700

    first commit

---
+++ inc/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+
+$konek = mysqli_connect("localhost", "codemaju_bansosadmin", "pmZUwe_yGW*{", "codemaju_bansos") or die("Gagal konek ke server MySQL" .mysql_error());
+// $bukadb = mysql_select_db($database) or die("Gagal membuka database $database" .mysql_error());
+
+// mysqli_connect()
+?>
